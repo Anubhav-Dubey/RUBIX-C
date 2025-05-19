42056b351948e873a9463f94973de9d537b92a65
anubhav dubey <anubhavdubey2468@gmail.com>
2025-05-19 12:36:36 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,14 @@
-const CACHE_NAME = 'rubix-c-v5';
+const CACHE_NAME = 'rubiks-cube-timer-v1';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './timer.js',
+  './cube.js',       // Added the new cube.js file
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'  // External lib
 ];
 
 self.addEventListener('install', (e) => {
@@ -21,7 +23,7 @@ self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request)
       .then(cached => cached || fetch(e.request))
-      .catch(() => caches.match('./index.html'))
+      .catch(() => caches.match('./index.html'))  // Fallback
   );
 });
 
